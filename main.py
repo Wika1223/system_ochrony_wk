e1f9b41626b8cc9a31af9883adf250b2c156e685
Wika1223 <u73515@student.wat.edu.pl>
2026-05-25 20:46:35 +0200

    wip: prepared read funcionality

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ companies = [
     ["Juwentus", "Gdańsk"]
 ]
 
-for c in companies:
+def read_data_company(company_data: list) -> None:
+    for c in company_data:
         print(f'Firma ochroniarska {c[0]} z miejscowości {c[1]}.')
+read_data_company(companies)
